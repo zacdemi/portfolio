f08a38cb011d0309ede724f884788bda23ebbd94
Zac Demi <zac.demi@gmail.com>
2021-06-03 20:11:02 -0500

    review horizontal scroll buttons

--- src/components/reviews.js
+++ src/components/reviews.js
@@ -1,28 +1,75 @@
-import React from "react";
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import React, { useRef, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import { AntDesign } from "@expo/vector-icons";
 
 import { Colors, Typography } from "../styles";
 import { reviews } from "../../data/reviews.json";
+import { usePhoneDimensions } from "../hooks";
+
+const { mobile } = usePhoneDimensions();
 
 const Reviews = (props) => {
+  const flatListRef = useRef(null);
+  const [index, setIndex] = useState(0);
+
+  const handleScrollLeft = () => {
+    if (index > 0) {
+      flatListRef.current.scrollToIndex({ animated: true, index: index - 1 });
+      setIndex((index) => index - 1);
+    }
+  };
+
+  const handleScrollRight = () => {
+    if (index < reviews.length - 1) {
+      flatListRef.current.scrollToIndex({ animated: true, index: index + 1 });
+      setIndex((index) => index + 1);
+    }
+  };
+
   return (
-    <FlatList
-      data={reviews}
-      keyExtractor={(item) => item.toString()}
-      horizontal
-      showsHorizontalScrollIndicator={false}
-      renderItem={({ item, index }) => {
-        return (
-          <View style={styles.card}>
-            <Text style={styles.text} adjustsFontSizeToFit>
-              {item.quote}
-            </Text>
-            <Text style={styles.name}>- {item.name}</Text>
-            <Text style={styles.title}>{item.title}</Text>
-          </View>
-        );
-      }}
-    />
+    <View>
+      <FlatList
+        ref={flatListRef}
+        data={reviews}
+        keyExtractor={(item) => item.toString()}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item, index }) => {
+          return (
+            <View style={styles.card}>
+              <Text style={styles.text} adjustsFontSizeToFit>
+                {item.quote}
+              </Text>
+              <Text style={styles.name}>- {item.name}</Text>
+              <Text style={styles.title}>{item.title}</Text>
+            </View>
+          );
+        }}
+      />
+      {!mobile && (
+        <TouchableOpacity
+          style={styles.caretRight}
+          onPress={() => handleScrollRight()}
+        >
+          <AntDesign name="caretright" size={40} color={Colors.tertiary} />
+        </TouchableOpacity>
+      )}
+
+      {!mobile && (
+        <TouchableOpacity
+          style={styles.caretLeft}
+          onPress={() => handleScrollLeft()}
+        >
+          <AntDesign name="caretleft" size={40} color={Colors.tertiary} />
+        </TouchableOpacity>
+      )}
+    </View>
   );
 };
 
@@ -48,6 +95,18 @@ const styles = StyleSheet.create({
     ...Typography.base,
     fontSize: 15,
     color: Colors.primary,
+  },
+  caretRight: {
+    position: "absolute",
+    top: "40%",
+    right: 5,
+    opacity: 0.4,
+  },
+  caretLeft: {
+    position: "absolute",
+    top: "40%",
+    left: 5,
+    opacity: 0.4,
   },
 });
 
